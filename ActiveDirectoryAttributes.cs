b35e50c371d166a281dd65c5cd65d5a2b5f7bbe0
Matteo Dello Ioio <matteo.delloioio@gmail.com>
2018-12-13 11:11:30 +0100

    Create ActiveDirectoryAttributes.cs

---
+++ ActiveDirectoryAttributes.cs
@@ -0,0 +1,140 @@
+struct ActiveDirectoryAttributes
+    {
+        /// <summary>
+        /// The logon name used to support clients and servers.
+        /// </summary>
+        public const string SAMAccountName = "sAMAccountName";
+
+        /// <summary>
+        /// The name that represents an object. Used to perform searches.
+        /// </summary>
+        public const string CommonName = "cn";
+
+        /// <summary>
+        /// The user's company name.
+        /// </summary>
+        public const string Company = "company";
+
+        /// <summary>
+        /// Contains the name for the department in which the user works.
+        /// </summary>
+        public const string Department = "department";
+
+        /// <summary>
+        /// Contains the given name (first name) of the user.
+        /// </summary>
+        public const string GivenName = "givenname";
+
+        /// <summary>
+        /// Contains the given name (first name) of the user.
+        /// </summary>
+        public const string FirstName = GivenName;
+
+        /// <summary>
+        /// This attribute contains the family or last name for a user.
+        /// </summary>
+        public const string Surname = "sn";
+
+        /// <summary>
+        /// This attribute contains the family or last name for a user.
+        /// </summary>
+        public const string LastName = Surname;
+
+        /// <summary>
+        /// The name of the organizational unit.
+        /// </summary>
+        public const string OrganizationalUnitName = "ou";
+
+        /// <summary>
+        /// Contains the distinguished name of the user who is the user's manager. 
+        /// The manager's user object contains a directReports property that contains references to all user objects that have their manager properties set to this distinguished name.
+        /// </summary>
+        public const string Manager = "manager";
+
+        /// <summary>
+        /// The user's email address.
+        /// </summary>
+        public const string Email = "mail";
+
+        /// <summary>
+        /// The primary mobile phone number.
+        /// </summary>
+        public const string MobilePhone = "mobile";
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute1 = "extensionAttribute1";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute2 = "extensionAttribute2";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute3 = "extensionAttribute3";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute4 = "extensionAttribute4";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute5 = "extensionattribute5";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute6 = "extensionAttribute6";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute7 = "extensionAttribute7";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute8 = "extensionAttribute8";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute9 = "extensionAttribute9";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute10 = "extensionAttribute10";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute11 = "extensionAttribute11";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute12 = "extensionAttribute12";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute13 = "extensionAttribute13";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute14 = "extensionAttribute14";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ExtesionAttribute15 = "extensionAttribute15";
+
+    }
